a09af8de3d1e393d82548e535d263bdd7aa46ff4
ahmed nasser <ahmednas197@gmail.com>
2021-02-12 18:06:39 +0200

    setup db folder and connect postgres database to the server

---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "yelp",
+  password: "Nasser2020",
+  port: "5432",
+});
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 const { json } = require("express");
 const express = require("express");
 const morgan = require("morgan");
-
+const db = require("./db");
 const app = express();
 app.use(express.json()); /// get the body sotred >>> not only sotored in the body , it takes json that we take from the client and covert it  to stander js object that we can manipulte where we wnat
 
 // get all the restaurants
-app.get("/api/v1/restaurants", (req, res) => {
-  console.log(req.params);
+app.get("/api/v1/restaurants", async (req, res) => {
+  const results = await db.query("select * from restaurants");
+
+  console.log(results);
   res.status(200).json({
     // get back json
     status: "success",
