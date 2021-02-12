2b776db76879debb03c928a1e71b0bf68e0c3df1
ahmed nasser <ahmednas197@gmail.com>
2021-02-12 18:16:51 +0200

    change the conncetion of Database from hard coded

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,6 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "yelp",
-  password: "Nasser2020",
-  port: "5432",
-});
+const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
